4927d96cf8d37a246d776f567e86b6ef5535c30d
vodohran <shmeihel@gmail.com>
2021-09-21 22:46:40 +0300

    add problem 9

---
+++ LeetCode/9. Palindrome Number.py
@@ -0,0 +1,16 @@
+﻿"""
+https://leetcode.com/problems/palindrome-number/
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+For example, 121 is palindrome while 123 is not.
+
+"""
+def isPalindrome(x):
+        return  str(x) == str(x)[::-1]
+
+print (isPalindrome(121))
+print (isPalindrome(-121))
+print (isPalindrome(13221))
+print (isPalindrome(14541))
