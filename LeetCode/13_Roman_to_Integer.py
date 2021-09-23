29fb3a40227bec423837ab2282b0bc3b7a6c0668
vodohran <shmeihel@gmail.com>
2021-09-24 00:03:55 +0300

    add problem 13

---
+++ LeetCode/13_Roman_to_Integer.py
@@ -0,0 +1,20 @@
+﻿# https://leetcode.com/problems/roman-to-integer/
+
+def romanToInt(s):
+	Romans = {	"I":1, 
+			"V":5,
+			"X":10,
+			"L":50,
+			"C":100,
+			"D":500,
+			"M":1000}
+	s = s[::-1]
+	sum = Romans[s[0]]
+	for i in range(1,len(s)):
+		if Romans[s[i]] >= Romans[s[i-1]]:
+			sum = sum + Romans[s[i]]
+		else:
+			sum = sum - Romans[s[i]]
+	return sum
+
+print(romanToInt("IX"))
