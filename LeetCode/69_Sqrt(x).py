b9b84061eca7a6a1663d9afd3619490bb205c5ce
vodohran <shmeihel@gmail.com>
2021-09-25 23:59:19 +0300

    add 69 problem

---
+++ LeetCode/69_Sqrt(x).py
@@ -0,0 +1,9 @@
+﻿# https://leetcode.com/problems/sqrtx/
+
+def mySqrt(x):
+        A = x // 2
+        while (x - A*A)<0:
+            A = (A + x // A) // 2
+        return A  
+
+print (mySqrt(170))
