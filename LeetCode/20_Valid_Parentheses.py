907bcd089b72761884bbd5dea3c4454de5c1a7a8
vodohran <shmeihel@gmail.com>
2021-09-25 17:02:47 +0300

    add 20 problems, 2 variant

---
+++ LeetCode/20_Valid_Parentheses.py
@@ -0,0 +1,34 @@
+﻿# def isValid(s):
+# 
+# 	
+# 	for i in range(len(s)):
+# 		s = s.replace("()",'')
+# 		s = s.replace("{}",'')
+# 		s = s.replace("[]",'')
+# 	return stack == []
+
+
+def isValid(s):
+	stack = []
+	
+	skobka_close = {")":"(", "}":"{","]":"["}
+	for i in s:
+		
+		if (i in skobka_close) and (len(stack)>0) and (stack[-1] == skobka_close[i]):
+			stack.pop()
+		else:
+			stack.append(i)
+
+	print(stack)		
+	return stack == []
+
+
+
+print(isValid("()"))
+print(isValid("()[]{}"))
+print(isValid("(]"))
+print(isValid("([)]"))
+print(isValid("{[]}"))
+print(isValid("]))"))
+
+
