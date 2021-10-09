b6643bc744396d6796caadba904d84682079bf1a
vodohran <shmeihel@gmail.com>
2021-10-10 01:26:39 +0300

    add 67 problem

---
+++ LeetCode/67_Add_Binary.py
@@ -0,0 +1,9 @@
+﻿# https://leetcode.com/problems/add-binary/
+
+    def addBinary(a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return str(bin(int(a,2) + int(b,2)))[2:]
