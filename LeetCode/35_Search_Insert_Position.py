dee9bbdce287ab1fbcec1a337f5c096f4ad2a3fd
vodohran <shmeihel@gmail.com>
2021-10-10 00:34:19 +0300

    add 35 problem

---
+++ LeetCode/35_Search_Insert_Position.py
@@ -0,0 +1,16 @@
+﻿def searchInsert( nums, target):
+        current_index = 0
+        left_index = 0
+        right_index = len(nums)-1
+        while (left_index <= right_index):
+                mid = (left_index+right_index) // 2
+                if target == nums[mid]: return mid
+                if target > nums[mid]:
+                        left_index = mid + 1
+                else:
+                        right_index = mid - 1
+        return left_index
+
+nums = [1,3,5,6]
+target = 5
+print(searchInsert(nums,target))
