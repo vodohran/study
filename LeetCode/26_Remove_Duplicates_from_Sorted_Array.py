10261212643feb0692953e522a60d71b422b2f17
vodohran <shmeihel@gmail.com>
2021-10-25 14:35:11 +0300

    Added problem 26

---
+++ LeetCode/26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,9 @@
+﻿def removeDuplicates( nums):
+    count = 0
+    for i in range(1,len(nums)):
+        if nums[count] < nums[i]:
+                count = count +1
+                nums[count] = nums[i]
+      
+    return count+1
+
