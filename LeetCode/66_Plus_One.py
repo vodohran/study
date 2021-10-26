343a5f320d993584f8d076b5d753f8cc78cf64a1
vodohran <shmeihel@gmail.com>
2021-10-26 23:34:34 +0300

    add problem 66

---
+++ LeetCode/66_Plus_One.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if (digits[i])==9:
+                digits[i]=0
+            else:
+                digits[i]=digits[i]+1
+                break
+                
+        if digits[0]==0:
+            digits.insert(0,1)
+            
+        return digits
